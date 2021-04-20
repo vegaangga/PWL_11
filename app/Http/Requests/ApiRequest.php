77aa43cec5ec2a8f5b84adc9b9c3e85d19c1cc24
vegaangga <vegaangga27@gmail.com>
2021-04-20 14:22:37 +0700

    Prak 1 Langkah 12 Ubah kode pada ApiRequest. class ApiRequest diganti menjadi abstract class ApiRequest

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\ApiResponse;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
-class ApiRequest extends FormRequest
+abstract class ApiRequest extends FormRequest
 {
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY
+        ));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -21,10 +44,5 @@ class ApiRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+
 }
