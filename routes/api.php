c1dc8bb9aeee1c8dc0afc41a6858436376e14022
vegaangga <vegaangga27@gmail.com>
2021-04-20 15:15:45 +0700

    Prak 1 Langkah 26 - implementasi fitur logout Selesai

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout',[AuthController::class,'logout']);
+});
