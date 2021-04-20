21ac08fbf9e7bac91265cc2621cd75b4b7a43f38
vegaangga <vegaangga27@gmail.com>
2021-04-20 14:23:08 +0700

    Prak 1 Langkah 13 Buat controller untuk menangani autentikasi dengan perintah artisan

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+}
