04d9bc3bde85ee126070ac28ecc94ef41731adae
vegaangga <vegaangga27@gmail.com>
2021-04-20 18:07:11 +0700

    Prak 2 Langkah 5 Buka model Todo dan lengkapi definisi relasi dengan model User. Lengkapi atribut fillable. Informasi kolom user_id tidak dibutuhkan sehingga perlu disembunyikan dari response. Dikarenakan representasi tipe data boolean pada database bernilai angka (0 dan 1), tambahkan casting atribut done dengan boolean.

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'user_id', 'todo','label','done'
+    ];
+
+    protected $hidden = [
+        'user_id'
+    ];
+
+    protected $casts =[
+        'done' => 'boolean'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
